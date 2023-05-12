b031f03f751a582b45ae5c9bd39c15ed2df4fb4b
jimin <thesuffering@naver.com>
2023-05-12 09:39:45 +0900

    2023-05-12
    전방향성 인공신경망 최초 커밋

---
+++ NDNNSolution/NDNN/Non_Directional_Neural_Network.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDNN
+{
+    public class Non_Directional_Neural_Network
+    {
+    }
+}
