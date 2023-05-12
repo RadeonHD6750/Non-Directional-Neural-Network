8286745449de198dd804ee41f1f587c780b58964
jimin <thesuffering@naver.com>
2023-05-12 12:08:40 +0900

    Neuron 추가

---
+++ NDNNSolution/NDNN/Mesh.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDNN
+{
+    public class Mesh
+    {
+        Dictionary<int, Neuron> neuronList;
+        int[] routingTable;
+
+        public Mesh() 
+        {
+        }
+    }
+}
---
+++ NDNNSolution/NDNN/Neuron.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDNN
+{
+    public class Neuron
+    {
+        private int id;
+        private double outputSignal;
+
+        Dictionary<int, double> inputSignalTable;
+        Dictionary<int, double> inputWeightTable;
+
+        public Neuron(List<int> connectedNeuronList) 
+        {
+            
+        }
+    }
+}
